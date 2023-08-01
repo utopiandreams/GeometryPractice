3c2bd79dbe02e8c494bed982d948e3520bf192f0
utopiandreams <berserkppp@naver.com>
2023-08-01 20:50:09 +0900

    스프링 프로젝트 로드

---
+++ src/test/java/practice/Geomquery/KihongApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.Geomquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KihongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
